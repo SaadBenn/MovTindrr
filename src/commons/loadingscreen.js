50c0b6ba536b28e0db894c326cab10d663e4f251
SaadBenn <saadbennmushtaq@gmail.com>
2018-11-02 22:14:58 -0400

    Set up style sheet for the screen loading

--- src/commons/loadingscreen.js
+++ src/commons/loadingscreen.js
@@ -19,26 +19,41 @@ class LoadingScreen extends Component {
     setToggleTimeout() {
         this._timer = setInterval(() => {
             if (this.state.dots.length === 3) {
-                this.setState({ dots: []});
+                this.setState({dots: []});
             } else {
-                this.setState({ dots: [...this.state.dots, '.'] });
+                this.setState({dots: [...this.state.dots, '.']});
             }
         }, 1000);
     }
 
     render() {
         return (
-          <View style={styles.root}>
-            <ActivityIndicator
-                animating
-                size=large
-                color={Colors.redColor}
-                style={styles.loader}
-            />
-              <Text style={styles.textStyle}>Loading{this.state.dots}</Text>
-          </View>
+            <View style={styles.root}>
+                <ActivityIndicator
+                    animating
+                    size=large
+                    color={Colors.redColor}
+                    style={styles.loader}
+                />
+                <Text style={styles.textStyle}>Loading{this.state.dots}</Text>
+            </View>
         );
     }
-
-    
 }
+
+    const styles =EStyleSheet.create({
+       root: {
+           flex: 1,
+           backgroundColor: '$blackColor',
+           justifyContent: 'center',
+           alignItems: 'center',
+       },
+       textStyle: {
+           fontFamily: 'montserrat-bold',
+           color: '#fff',
+           fontSize: 25
+       },
+       loader: {height :75}
+    });
+
+    export default LoadingScreen;
