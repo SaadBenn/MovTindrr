839262c6f19c4332b17a74756eb4ba69d63b0a42
SaadBenn <saadbennmushtaq@gmail.com>
2018-11-03 20:10:25 -0400

    Set up reducer

---
+++ src/modules/home/reducer.js
@@ -0,0 +1,20 @@
+import {GET_MOVIES_POPULAR} from "./actions";
+
+const INITIAL_STATE = {
+    isFetched: false,
+    error: null,
+    movies: null
+};
+
+export default (state = INITIAL_STATE) => {
+    switch (action.type) {
+        case '${GET_MOVIES_POPULAR}_PENDING':
+            return INITIAL_STATE;
+        case '${GET_MOVIES_POPULAR}_FULFILLED':
+            return { ...state, isFetched: true, movies: action.payload.results};
+        case '${GET_MOVIES_POPULAR}_REJECTED':
+            return {...state, isFetched: true, error: action.payload};
+        default:
+            return state;
+    }
+};
